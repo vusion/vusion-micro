6352bd3ad5bd15b2f32e9c5a2999baa233734e0e
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-06 20:16:39 +0800

    support registerApp unloadApp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import micro, { SubApp } from './init';
 import { wrapReturnPromise } from './utils';
-import { registerApplication, start } from 'single-spa';
+import { registerApplication, start, getAppNames, unloadApplication } from 'single-spa';
 import { publish, subscribe, clearTopic } from 'vusion-micro-data';
 import loadEntry from './loadEntry';
 type AppConfigs = {
@@ -23,49 +23,64 @@ type App = {
     entries: SubApp["entries"];
 } & AppConfig;
 const registerApp = function (app: App): void {
-    registerApplication(app.name, () => {
-        const topic = 'app:' + app.name;
-        return Promise.resolve({
-            bootstrap(): Promise<any> {
-                return loadEntry(app.entries, app.name).then(() => wrapReturnPromise(app.bootstrap));
-            },
-            mount(customProps): Promise<any> {
-                clearTopic(topic + ':unmounted');
-                return new Promise((res, rej): void => {
-                    const done = function (): void {
-                        const clear = publish(topic + ':mount', {
-                            customProps,
-                        });
-                        clear();
-                        subscribe(topic + ':mounted', (): void => {
-                            wrapReturnPromise(app.mounted).then(res, rej);
-                        }, true);
-                    };
-                    wrapReturnPromise(app.mount).then(done, rej);
-                });
-            },
-            unmount(customProps): Promise<any> {
-                clearTopic(topic + ':mounted');
-                return new Promise((res, rej): void => {
-                    const done = function (): void {
-                        const clear = publish(topic + ':unmount', {
-                            customProps,
-                        });
-                        clear();
-                        subscribe(topic + ':unmounted', (): void => {
-                            wrapReturnPromise(app.unmounted).then(res, rej);
-                        }, true);
-                    };
-                    wrapReturnPromise(app.unmount).then(done, rej);
-                });
-            },
-        });
-    }, app.isActive, app.customProps);
+    const customProps = app.customProps;
+    if (getAppNames().includes(app.name)) {
+        console.warn('repeat register:' + app.name);
+        return;
+    }
+    registerApplication({
+        name: app.name,
+        app: () => {
+            const topic = 'app:' + app.name;
+            return Promise.resolve({
+                bootstrap(): Promise<any> {
+                    return loadEntry(app.entries, app.name).then(() => wrapReturnPromise(app.bootstrap));
+                },
+                mount(customProps): Promise<any> {
+                    clearTopic(topic + ':unmounted');
+                    return new Promise((res, rej): void => {
+                        const done = function (): void {
+                            const clear = publish(topic + ':mount', {
+                                customProps,
+                            });
+                            clear();
+                            subscribe(topic + ':mounted', (): void => {
+                                wrapReturnPromise(app.mounted).then(res, rej);
+                            }, true);
+                        };
+                        wrapReturnPromise(app.mount).then(done, rej);
+                    });
+                },
+                unmount(customProps): Promise<any> {
+                    clearTopic(topic + ':mounted');
+                    return new Promise((res, rej): void => {
+                        const done = function (): void {
+                            const clear = publish(topic + ':unmount', {
+                                customProps,
+                            });
+                            clear();
+                            subscribe(topic + ':unmounted', (): void => {
+                                wrapReturnPromise(app.unmounted).then(res, rej);
+                            }, true);
+                        };
+                        wrapReturnPromise(app.unmount).then(done, rej);
+                    });
+                },
+            });
+        },
+        activeWhen: app.isActive,
+        customProps,
+    });
+};
+const unloadApp = function(name: string): ReturnType<typeof unloadApplication> {
+    return unloadApplication(name);
 };
 export default {
     getEntries(name: string): Promise<SubApp[]> {
         return Promise.resolve(micro.config[name]);
     },
+    unloadApp,
+    registerApp,
     registerApps(appConfigs: AppConfigs, appEntries: SubApp[]): void {
         appEntries.forEach((item: SubApp): void => {
             const name = item.name;
